b02810fa2b60258b254a10b81e0f814c2b6093c1
bd240897 <bd240897@yandex.ru>
2022-11-13 11:35:33 +0300

    dropped old files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,9 @@
 <template>
-<!--  <PageLoader />-->
-<!--  <nav>-->
-<!--    <router-link :to="{ name: 'Login'}">Login</router-link> |-->
-<!--    <router-link :to="{ name: 'ProfileView'}">ProfileView</router-link> |-->
-<!--    <router-link :to="{ name: 'ChatView'}">ChatView</router-link> |-->
-<!--    <router-link :to="{ name: 'TestView', params: { id: 1 }}">TestView</router-link> |-->
-<!--  </nav>-->
   <router-view/>
 </template>
 
 <style>
-  /*необходимо для центрации дочерних элементов по середине экрана*/
+
   html, body {
     height: 100%;
   }
@@ -19,14 +12,9 @@
     height: 100%;
   }
 
-  /*#app{*/
-  /*  background-color: #3CB5E8;*/
-  /*}*/
-
 </style>
 
 <script>
-import Menu from '@/components/Menu.vue';
 import PageLoader from './components/PageLoader'
 export default {
   name: 'App',
